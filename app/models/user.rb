a535116337ca582f6bf7c93e58319cdf1c1b85a8
tomatoman2 <kasuga01m@gmail.com>
2020-04-19 03:55:47 +0900

    add user

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, uniqueness: true
+  # validates :user_id, {presence: true}
+  # def user
+  #   return User.find_by(id: self.user_id)
+  # end
+end
